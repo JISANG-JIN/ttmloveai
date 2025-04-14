edcea879991bfc2f335ae4daeead5658fea97aa8
JISANG-JIN <zzonbug333@naver.com>
2025-04-14 21:48:08 +0900

    Update ColumnSignupPage.tsx

--- app/column/ColumnSignupPage.tsx
+++ app/column/ColumnSignupPage.tsx
@@ -209,7 +209,7 @@ export default function ColumnSignupPage() {
               <Facebook className="h-4 w-4 text-white" />
             </a>
             <a
-              href="https://instagram.com"
+              href="https://www.instagram.com/ttmloveai/"
               target="_blank"
               rel="noopener noreferrer"
               className="social-icon-btn w-9 h-9"
