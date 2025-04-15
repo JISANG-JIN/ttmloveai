fbad3111f7847dfb2210ef339e30e7bb546108fa
JISANG-JIN <zzonbug333@naver.com>
2025-04-15 22:12:23 +0900

    Update page.tsx

--- app/legal/cookies/page.tsx
+++ app/legal/cookies/page.tsx
@@ -197,7 +197,7 @@ export default function CookiePolicyPage() {
           </div>
           <div className="flex justify-center gap-3 mb-4">
             <a
-              href="https://facebook.com"
+              href="https://www.facebook.com/share/18hWcMxFH9/?mibextid=wwXIfr"
               target="_blank"
               rel="noopener noreferrer"
               className="social-icon-btn w-9 h-9"
